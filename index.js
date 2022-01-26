cb459e8986117edbacf65d4ebe9a9901992ce0a2
Eduardo Sierra <jeduardo.robles127@gmail.com>
2022-01-25 18:06:57 -0600

    Ya que quedo mi parte

---
+++ index.js
@@ -0,0 +1,10 @@
+let estudiante2 = {
+    name: "Eduardo",
+    apelidos: "Sierra Robles",
+    anios: 23
+}
+console.log(estudiante2);
+
+let diferenciaAnios = function(estudiante1, estudiante2){
+    return estudiante1 - estudiante2
+}
